bfc625f333e2f73380f2f10b1fff3cf1d4e7e028
pbfrias17 <pbfrias17@gmail.com>
2014-11-03 23:21:27 -0500

    cmon

--- Proj3/HashedSplays.cpp
+++ Proj3/HashedSplays.cpp
@@ -11,9 +11,12 @@ HashedSplays::HashedSplays(const int &size)
 void HashedSplays::FileReader(const string &filename) {
 	ifstream file;
 	if(Util::FileExists(filename)) {
+		cout << "Creating Splay Tree from file: " << filename << endl;
 		file.open(filename.c_str(), ios_base::in);
+		
+	} else {
+		cout << filename << " could not be found.\n";
 	}
-	cout << "Creating Splay Tree from file: " << filename << endl;
 
 	file.close();
 }
